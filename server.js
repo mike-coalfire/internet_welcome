6d96a7b88a5a4d116815edd5b8c2cbe590dd0dcf
Tilman Holschuh <tilman@heroku.com>
2025-01-09 10:03:12 -0800

    Add code
    Add .gitignore
    
    Remove node_modules

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+
+const requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/submit', (req, res) => {
+  const name = req.body.name;
+  console.log(`Received request from ${req.ip} with browser ${req.headers['user-agent']}`);
+  console.log(`Time to display page: ${Date.now() - req.requestTime} ms`);
+  res.send(`<html><body>Welcome, ${name}!<p>IP: ${req.ip}<p>Headers: ${req.headers['user-agent']}<p>Time: ${Date.now() - req.requestTime} ms</body></html>`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html',{ root: __dirname });
+});
+
+app.listen(8000, () => {
+  console.log('Server started on port 8000');
+});
